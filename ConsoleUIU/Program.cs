a6709acadbeca961541efa241e08666d276a3d48
sumeyrakarsavran <bir-kar-12@hotmail.com>
2021-02-21 17:11:49 +0300

    Add project files.

---
+++ ConsoleUIU/Program.cs
@@ -0,0 +1,97 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUIU //ConsoleUI olması gerektiğini biliyorum ama bir yanlışlık yaptığım için öyle yazamadım :)
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            Console.WriteLine("----CAR LIST----");
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine("CarId:" + car.CarId + " BrandId:" + car.BrandId + " ColorId:" + car.ColorId+ " DailyPrice:" + car.DailyPrice + " Description:" +car.Description + " ModelYear:" +car.ModelYear);
+            }
+            Console.WriteLine("\n\n");
+
+            Console.WriteLine("----GetById:1----");
+            foreach (var car in carManager.GetById(1))
+            {
+                Console.WriteLine("CarId:" + car.CarId + " BrandId:" + car.BrandId + " ColorId:" + car.ColorId + " DailyPrice:" + car.DailyPrice + " Description:" + car.Description + " ModelYear:" + car.ModelYear);
+            }
+            Console.WriteLine("\n\n");
+
+            Console.WriteLine("----GetById:2----");
+            foreach (var car in carManager.GetById(2))
+            {
+                Console.WriteLine("CarId:" + car.CarId + " BrandId:" + car.BrandId + " ColorId:" + car.ColorId + " DailyPrice:" + car.DailyPrice + " Description:" + car.Description + " ModelYear:" + car.ModelYear);
+            }
+            Console.WriteLine("\n\n");
+
+            Console.WriteLine("----GetById:3----");
+            foreach (var car in carManager.GetById(3))
+            {
+                Console.WriteLine("CarId:" + car.CarId + " BrandId:" + car.BrandId + " ColorId:" + car.ColorId + " DailyPrice:" + car.DailyPrice + " Description:" + car.Description + " ModelYear:" + car.ModelYear);
+            }
+            Console.WriteLine("\n\n");
+
+            Console.WriteLine("----GetById:4----");
+            foreach (var car in carManager.GetById(4))
+            {
+                Console.WriteLine("CarId:" + car.CarId + " BrandId:" + car.BrandId + " ColorId:" + car.ColorId + " DailyPrice:" + car.DailyPrice + " Description:" + car.Description + " ModelYear:" + car.ModelYear);
+            }
+            Console.WriteLine("\n\n");
+
+            Console.WriteLine("----GetById:5----");
+            foreach (var car in carManager.GetById(5))
+            {
+                Console.WriteLine("CarId:" + car.CarId + " BrandId:" + car.BrandId + " ColorId:" + car.ColorId + " DailyPrice:" + car.DailyPrice + " Description:" + car.Description + " ModelYear:" + car.ModelYear);
+            }
+            Console.WriteLine("\n\n");
+
+            Console.WriteLine("----GetByBrandId:1----");
+            foreach (var car in carManager.GetByBrandId(1))
+            {
+                Console.WriteLine("CarId:" + car.CarId + " BrandId:" + car.BrandId + " ColorId:" + car.ColorId + " DailyPrice:" + car.DailyPrice + " Description:" + car.Description + " ModelYear:" + car.ModelYear);
+            }
+            Console.WriteLine("\n\n");
+
+            Console.WriteLine("----GetByBrandId:2----");
+            foreach (var car in carManager.GetByBrandId(2))
+            {
+                Console.WriteLine("CarId:" + car.CarId + " BrandId:" + car.BrandId + " ColorId:" + car.ColorId + " DailyPrice:" + car.DailyPrice + " Description:" + car.Description + " ModelYear:" + car.ModelYear);
+            }
+            Console.WriteLine("\n\n");
+
+            Console.WriteLine("----GetByBrandId:3----");
+            foreach (var car in carManager.GetByBrandId(3))
+            {
+                Console.WriteLine("CarId:" + car.CarId + " BrandId:" + car.BrandId + " ColorId:" + car.ColorId + " DailyPrice:" + car.DailyPrice + " Description:" + car.Description + " ModelYear:" + car.ModelYear);
+            }
+            Console.WriteLine("\n\n");
+
+            Console.WriteLine("----GetByColorId:1----");
+            foreach (var car in carManager.GetByColorId(1))
+            {
+                Console.WriteLine("CarId:" + car.CarId + " BrandId:" + car.BrandId + " ColorId:" + car.ColorId + " DailyPrice:" + car.DailyPrice + " Description:" + car.Description + " ModelYear:" + car.ModelYear);
+            }
+            Console.WriteLine("\n\n");
+
+            Console.WriteLine("----GetByColorId:2----");
+            foreach (var car in carManager.GetByColorId(2))
+            {
+                Console.WriteLine("CarId:" + car.CarId + " BrandId:" + car.BrandId + " ColorId:" + car.ColorId + " DailyPrice:" + car.DailyPrice + " Description:" + car.Description + " ModelYear:" + car.ModelYear);
+            }
+            Console.WriteLine("\n\n");
+
+            Console.WriteLine("----GetByColorId:3----");
+            foreach (var car in carManager.GetByColorId(3))
+            {
+                Console.WriteLine("CarId:" + car.CarId + " BrandId:" + car.BrandId + " ColorId:" + car.ColorId + " DailyPrice:" + car.DailyPrice + " Description:" + car.Description + " ModelYear:" + car.ModelYear);
+            }
+            Console.WriteLine("\n\n");
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,85 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        List<Color> _colors;
+        List<Brand> _brands;
+
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{CarId=1, BrandId=1, ColorId=2, DailyPrice=70000, ModelYear="2010", Description="ABC"},
+                new Car{CarId=2, BrandId=3, ColorId=3, DailyPrice=80000, ModelYear="2005", Description="XYZ"},
+                new Car{CarId=3, BrandId=2, ColorId=2, DailyPrice=60000, ModelYear="2007", Description="YTR"},
+                new Car{CarId=4, BrandId=1, ColorId=1, DailyPrice=66000, ModelYear="2009", Description="UIU"},
+                new Car{CarId=5, BrandId=3, ColorId=2, DailyPrice=76000, ModelYear="2003", Description="OPP"},
+            };
+
+            _colors = new List<Color>
+            {
+                new Color{ColorId=1, ColorName="White"},
+                new Color{ColorId=2, ColorName="Black"},
+                new Color{ColorId=3, ColorName="Red"},
+            };
+
+            _brands = new List<Brand>
+            {
+                new Brand{BrandId=1, BrandName="Hyundai"},
+                new Brand{BrandId=2, BrandName="Toyota"},
+                new Brand{BrandId=3, BrandName="BMW"}
+            };
+        }
+
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetByBrandId(int brandId)
+        {
+            return _cars.Where(c => c.BrandId == brandId).ToList();
+        }
+
+        public List<Car> GetByColorId(int colorId)
+        {
+            return _cars.Where(c => c.ColorId == colorId).ToList();
+        }
+
+        public List<Car> GetById(int carId)
+        {
+            return _cars.Where(c => c.CarId == carId).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.CarId == car.CarId);
+            carToUpdate.CarId = car.CarId;
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+            carToUpdate.ModelYear = car.ModelYear;
+        }
+    }
+}
